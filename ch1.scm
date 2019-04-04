47691d80781ef9dd07ddd7c6b1f0e9541b5ff209
Todd Igoe <vujadetech42@gmail.com>
2019-04-03 21:04:41 -0400

    Add Ex 1.8, Newton's method for cube roots

--- ch1.scm
+++ ch1.scm
@@ -1,5 +1,7 @@
 #lang racket
 
+; To run test code, uncomment the double semi-colons at the end of each section.
+
 (define xs '(42 99 7)) ; for testing
 
 (define (sq x) (* x x)) ; sq is an abbreviation for square 
@@ -8,7 +10,7 @@
 ; Ex 1.3 [list of 3 numbers] -> [sum of squares of the two larger nums]
 (define (ex1.3 xs)
   (apply + (map sq (remove (apply min xs) xs))))
-;(ex1.3 xs) ; '(42 99 7) -> 11565
+;; (ex1.3 xs) ; '(42 99 7) -> 11565
 
 ; *******************************************
 ; Ex 1.7 good-enough-rel? relative version
@@ -28,7 +30,6 @@
 (define (good-enough? guess x) 
   (< (abs (- (sq guess) x)) EPSILON))
 
-;
 (define (sqrt x) (sqrt-iter good-enough? 1.0 x))
 
 (define (good-enough-rel? guess x)
@@ -39,15 +40,39 @@
 (define (sqrt-rel x) (sqrt-iter good-enough-rel? 1.0 x))
 (define (sqrt-both x) (list (sqrt x) (sqrt-rel x)))
 (define (sqrt-both-errors x)
-  (let ([true-val (expt x 0.5)]
+  (let ([true-val (expt x 0.5)]  ; use built-in expt to get the true value
         [orig-val (sqrt x)]
-        [rel-val  (sqrt-rel x)]) ; use built-in expt to get the true value
+        [rel-val  (sqrt-rel x)])
     (list (list 'error-original-version (- true-val orig-val))
           (list 'error-relative-version (- true-val rel-val)))))
 
 (define SMALL_NUMBER_TO_TEST 0.01)
 ;; (sqrt-both-errors SMALL_NUMBER_TO_TEST) ; relative version gives an error that's smaller by several orders of magnitude
+
 ; *******************************************
+; Ex 1.8: Newton's method for cube roots
+(define (abs-diff x y) (abs (- x y))) ; absolute difference
+(define (cube x) (expt x 3))
+(define (true-cube-root x) (expt x 1/3))
+; The error calculation wasn't mentioned in this exercise, but I thought it'd be interesting to see.
+(define (error-cubert x) (abs-diff (true-cube-root x) (cubert x)))
+
+(define (good-enough-cubert? guess x) 
+  (< (abs (- (cube guess) x)) EPSILON))
+
+(define (cubert-iter pred guess x) ; pred is predicate test to be used
+  (if (pred guess x)
+      guess
+      (cubert-iter pred (improve-cubert guess x) x)))
+
+(define (improve-cubert guess x)
+  ; (x/guess^2 + 2*guess)/3
+  (/ (+ (/ x (sq guess)) (* 2 guess)) 3))
+
+(define (cubert x) (cubert-iter good-enough-cubert? 1.0 x))
+
+;; (cube (cubert 2)) ; 2.0000592593226547
+;; (cube (cubert 0.001)) ; 0.0013964147028898309 which is off by quite a bit
 ; *******************************************
 ; *******************************************
 ; *******************************************
