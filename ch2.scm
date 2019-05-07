8c0708c6702d6c956621856d212b9f5594c4dc6a
Todd Igoe <vujadetech42@gmail.com>
2019-05-07 01:47:31 -0400

    Add 2.56

--- ch2.scm
+++ ch2.scm
@@ -603,8 +603,77 @@
 ;; (vujadeTech-equal? '(this is a list) '(this is a list))   ; => #t
 ;; (vujadeTech-equal? '(this is a list) '(this (is a) list)) ; => #f
 ; *******************************************
+; Ex 2.55 I'm not sure why it prints back quote, but apparently ''a is a list,
+; so it has a car and cdr, unlike 'a.
 ; *******************************************
+; Sec 2.3.2 Symbolic differentiation
+
+(define (variable? x) (symbol? x))
+
+; Two variables are the same if the symbols representing them are eq?:
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+; Sums and products are constructed as lists:
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+
+; A sum is a list whose first element is the symbol +:
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+; The addend is the second item of the sum list:
+(define (addend s) (cadr s))
+
+; The augend is the third item of the sum list:
+(define (augend s) (caddr s))
+
+; A product is a list whose first element is the symbol *:
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+; The multiplier is the second item of the product list:
+(define (multiplier p) (cadr p))
+
+; The multiplicand is the third item of the product list:
+(define (multiplicand p) (caddr p))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        
+        ; added for Ex 2.56, exponentiation:
+        ((exponentiation? exp)
+         (let ([u (base exp)] [n (exponent exp)]) ; D(u^n)
+           (cond
+             [(= 0 n) 0]
+             [(= 1 n) (deriv u var)]
+             [else
+              (make-product (make-product n (make-exponentiation u (- n 1))) (deriv u var))]
+             )))
+
+        (else
+         (error "unknown expression type -- DERIV" exp))))
 ; *******************************************
+; Ex 2.56, see deriv for exponentiation? section above
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base exponentiation) (cadr exponentiation))
+(define (exponent exponentiation) (caddr exponentiation))
+
+(define (make-exponentiation b e) (list '** b e))
+
 ; *******************************************
 ; *******************************************
 ; *******************************************
