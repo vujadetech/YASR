717e40194fdf3b3cefdb4697bb01d4aa09aae380
Todd Igoe <vujadetech42@gmail.com>
2019-05-11 18:36:41 -0400

    Add 3.14

--- ch3-lang-sicp.scm
+++ ch3-lang-sicp.scm
@@ -17,14 +17,14 @@
 (define x (list 'a 'b))
 (define y (list 'c 'd))
 (define z (append x y))
-z
+;z
 ; (a b c d) normal append
-(cdr x)
+;(cdr x)
 ; <response> (b) like normal
 (define w (append! x y))
-w
+;w
 ; (a b c d)
-(cdr x)
+;(cdr x)
 ; <response> '(b c d) b/c x was changed so it pointed to y after the b.
 
 ; *******************************************
@@ -37,8 +37,19 @@ w
 ;;(cadddr z13) ; => 'a since it loops back to beginning after 3 cdrs,
 ; and will keep looping forever if (cdr z13) is called.
 
-
 ; *******************************************
+; Ex 3.14
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x) ; nothing to reverse, so...
+        y         ; ... return what's been accumulated.
+        (let ((temp (cdr x))) ; temp is what will be reversed next
+          (set-cdr! x y) ; y is accumulator so (cdr x) is what's been reversed so far, namely y.
+          (loop temp x)))) ; loop next part and accumulate it to x
+  (loop x '())) ; start by accumulating on empty list
+
+; It's reversing the list x with y serving as an internal accumulator.
+
 ; *******************************************
 ; *******************************************
 ; *******************************************
